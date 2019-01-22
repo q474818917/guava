68a8e018d7c20b42ec0b308700e99e9138d820fa
wangzhenxing04 <wangzhenxing04@meituan.com>
2019-01-22 17:32:31 +0800

    add LRUCache

---
+++ guava-sample/src/main/java/com/google/guava/cache/LRUCache.java
@@ -0,0 +1,43 @@
+package com.google.guava.cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private Map<Integer, Integer> map;
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+
+    public static void main(String args[]) {
+        LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       // 返回  1
+        cache.put(3, 3);    // 该操作会使得密钥 2 作废
+        cache.get(2);       // 返回 -1 (未找到)
+        cache.put(4, 4);    // 该操作会使得密钥 1 作废
+        cache.get(1);       // 返回 -1 (未找到)
+        cache.get(3);       // 返回  3
+        cache.get(4);       // 返回  4
+
+    }
+}
